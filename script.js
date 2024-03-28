c8313f33f4c233333460cf8734af4f8df59a0321
SuchitraRG <156281044+SuchitraRG@users.noreply.github.com>
2024-03-28 19:49:18 +0530

    i had made some changes in code

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-async function fetchWeatherData(cityName){
-	let url =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=77703ce485871286a0728454fb80d206`
-	const response = await fetch(url)
-	const data = await response.json();
+async function fetchWeatherCity(cityName){
+	let url=`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=2acbac7c6cedabb2fc01a529b86ede80`;
+	const response= await fetch(url)
+	const data= await response.json();
+	let city= document.getElementById("city");
+	city.innerHTML=data.name;
+	let windSpeed = document.getElementById("wind_speed");
+	windSpeed.innerHTML=data.wind.speed + "km/hr";
+	let humidity = document.getElementById("humidity");
+	humidity.innerHTML = data.main.humidity+"g/kg";
+	let temp = document.getElementById("temp");
+	temp.innerHTML = Math.round((data.main.temp)-273.15) +"°C";
 	console.log(data)
 }
+//demo()
+
 function fetchCity(){
-	let cityName = document.getElementById("city_name")
-	if(cityName.value==="")
-	{
-		alert("Enter a city name")
-	}
-	else
-	{
-		fetchWeatherData(cityName.value);
+	let cityName = document.getElementById("cityname");
+	if(cityName.value===""){
+		alert("Enter a city name");
+	}else{
+		fetchWeatherCity(cityName.value);
 		cityName.value="";
 	}
 }
